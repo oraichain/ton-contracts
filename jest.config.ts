211eab8360c6265c8c75e9af6d6efe7479e37b2c
Pham Tu <tubackkhoa@gmail.com>
2024-05-07 13:53:33 +0700

    change to swc/jest

--- jest.config.ts
+++ jest.config.ts
@@ -1,7 +1,9 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-    preset: 'ts-jest',
+    transform: {
+        '^.+\\.(t|j)sx?$': '@swc/jest',
+    },
     testEnvironment: 'node',
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
 };
